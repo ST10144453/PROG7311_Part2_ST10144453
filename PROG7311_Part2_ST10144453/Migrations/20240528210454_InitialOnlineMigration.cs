9aa515f52bf00872a13e9d1bf56b5e9ed97bcd87
Anneme Holzhausen <ST10144453@vcconnect.edu.za>
2024-05-28 23:14:47 +0200

    ONLINE DATABASE MOTHER FUCKERS

---
+++ PROG7311_Part2_ST10144453/Migrations/20240528210454_InitialOnlineMigration.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PROG7311_Part2_ST10144453.Migrations
+{
+    public partial class InitialOnlineMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
